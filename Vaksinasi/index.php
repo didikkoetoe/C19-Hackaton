3188365195d58e8ae4f530238a32117f0b6df5aa
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-24 09:04:20 +0700

    Create file vaksinasi.php

---
+++ Vaksinasi/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <!-- CSS login -->
+    <link rel="stylesheet" href="../css/vaksinasi.css">
+
+    <title>Vaksinasi</title>
+</head>
+
+<body>
+    <h1>Daftar Vaksinasi</h1>
+
+
+    <!-- Javascript -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Javascript -->
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- jS login -->
+    <script src=""></script>
+    <!-- End of javascript -->
+
+</body>
+
+</html>
