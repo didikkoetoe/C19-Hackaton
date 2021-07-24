3b1e80f23a7cd59b355d8037918126cb5071f6a0
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-24 08:24:26 +0700

    membuat navbar

--- index.php
+++ index.php
@@ -13,11 +13,46 @@
     <!-- CSS login -->
     <link rel="stylesheet" href="css/index.css">
 
-    <title>Hello, world!</title>
+    <title>C19</title>
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+    <!-- Navbar -->
+    <section class="navigasi">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">C19</span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Statistik</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">List</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Edukasi</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Apa itu Covid ?</a></li>
+                                <li><a class="dropdown-item" href="#">Berita Hoax yang beredar</a></li>
+                                <li><a class="dropdown-item" href="#">Cara Mencegah</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <form class="d-flex" id="cari">
+                    <input class="form-control me-2 rounded-pill" type="search" placeholder="Cari" aria-label="Search">
+                </form>
+            </div>
+        </nav>
+    </section>
+    <!-- End of navbar -->
 
 
     <!-- Javascript -->
