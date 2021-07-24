dbe0274f593f921f00b48c2063bb446c8ce11415
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-24 10:20:44 +0700

    Connect to API and using

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+$json = file_get_contents("https://data.covid19.go.id/public/api/update.json");
+$data = json_decode($json, true);
+$penambahan = $data["update"]["penambahan"];
+$total = $data["update"]["total"];
+
+
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -18,42 +28,112 @@
 
 <body>
     <!-- Navbar -->
-    <section class="navigasi">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-            <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">C19</span>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Statistik</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">List</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Edukasi</a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="#">Apa itu Covid ?</a></li>
-                                <li><a class="dropdown-item" href="#">Berita Hoax yang beredar</a></li>
-                                <li><a class="dropdown-item" href="#">Cara Mencegah</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+                <div class="container-fluid">
+                    <span class="navbar-brand mb-0 h1">C19</span>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Statistik</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">List</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="vaksinasi/index.php">Vaksinasi</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Edukasi</a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Apa itu Covid ?</a></li>
+                                    <li><a class="dropdown-item" href="#">Berita Hoax yang beredar</a></li>
+                                    <li><a class="dropdown-item" href="#">Cara Mencegah</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <form class="d-flex" id="cari">
+                        <input class="form-control me-2 rounded-pill" type="search" placeholder="Cari" aria-label="Search">
+                    </form>
                 </div>
-                <form class="d-flex" id="cari">
-                    <input class="form-control me-2 rounded-pill" type="search" placeholder="Cari" aria-label="Search">
-                </form>
-            </div>
-        </nav>
-    </section>
+            </nav>
+        </div>
+    </div>
     <!-- End of navbar -->
 
+    <!-- Statistik -->
+    <div class="container-fluid">
+        <div class="row mt-3">
+            <div class="col-md-8">
+                <!-- Heading -->
+                <div class="h2 text-center">Statistik</div>
+                <hr>
+                <!-- End of heading -->
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card mb-3" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">Penambahan Kasus Harian</h5>
+                                <ul class="list-group">
+                                    <li class="list-group-item list-group-item-danger">Jumlah Positif : <?= $penambahan["jumlah_positif"]; ?></li>
+                                    <li class="list-group-item list-group-item-warning">Jumlah Dirawat : <?= $penambahan["jumlah_dirawat"]; ?></li>
+                                    <li class="list-group-item list-group-item-dark">Jumlah Meninggal : <?= $penambahan["jumlah_meninggal"]; ?></li>
+                                    <li class="list-group-item list-group-item-success">Jumlah Sembuh : <?= $penambahan["jumlah_sembuh"]; ?></li>
+                                    <li class="list-group-item"><i class="fas fa-calendar"></i> Data Tanggal : <?= $penambahan["tanggal"]; ?></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-3" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Total Kasus</h5>
+                                <ul class="list-group">
+                                    <li class="list-group-item list-group-item-danger">Jumlah Positif : <?= $penambahan["jumlah_positif"]; ?></li>
+                                    <li class="list-group-item list-group-item-warning">Jumlah Dirawat : <?= $penambahan["jumlah_dirawat"]; ?></li>
+                                    <li class="list-group-item list-group-item-dark">Jumlah Meninggal : <?= $penambahan["jumlah_meninggal"]; ?></li>
+                                    <li class="list-group-item list-group-item-success">Jumlah Sembuh : <?= $penambahan["jumlah_sembuh"]; ?></li>
+                                    <li class="list-group-item"><i class="fas fa-calendar"></i> Data Tanggal : <?= $penambahan["tanggal"]; ?></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-3" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">Total Kasus</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 border">
+                <!-- <div class="smallStatistic">
+                    <ul class="list-group">
+                        <li class="list-group-item active" aria-current="true">An active item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                        <li class="list-group-item">A fourth item</li>
+                        <li class="list-group-item">And a fifth one</li>
+                    </ul>
+                </div> -->
+            </div>
+        </div>
+    </div>
+    <!-- End of statistik -->
 
     <!-- Javascript -->
     <!-- Option 1: Bootstrap Bundle with Popper -->
