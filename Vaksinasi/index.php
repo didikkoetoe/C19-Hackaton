14b0a6d8ebae08648bc3d6d2b33f112b2890ab5e
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-24 10:21:22 +0700

    Edit buttons

--- Vaksinasi/index.php
+++ Vaksinasi/index.php
@@ -58,15 +58,11 @@
             <div class="mb-2">
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                    <label class="form-check-label" for="flexRadioDefault1">
-                        Default radio
-                    </label>
+                    <label class="form-check-label" for="flexRadioDefault1">Laki-laki</label>
                 </div>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-                    <label class="form-check-label" for="flexRadioDefault2">
-                        Default checked radio
-                    </label>
+                    <label class="form-check-label" for="flexRadioDefault2">Perempuan</label>
                 </div>
             </div>
 
